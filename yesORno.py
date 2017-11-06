6bdbbc95f56254a082f03fac127cc0f520b8f517
Freeze <120677982@qq.com>
2017-11-07 01:02:56 +0800

    add yesORno.py

---
+++ yesORno.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+while True:
+    answer = raw_input("please input yes or no:")
+    if answer == "yes":
+        print "you choiced yes!"
+        break
+    elif answer == "no":
+        print "you choices no!"
+        break
+
